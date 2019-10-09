90898e501aa488423a2eff57ab861640fb725bf7
aryachiranjeev <31439731+aryachiranjeev@users.noreply.github.com>
2019-10-09 19:26:34 +0530

    Create interesting_output_qs.c

---
+++ interesting_output_qs.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main(){
+float i=0.11;
+float j=0.6;
+if(i<0.11){
+printf("Let");
+}
+else if(i>=0.11){
+printf("us");
+}
+
+if(j<0.6){
+printf("us");
+}
+else if(j>0.6){
+printf("C");
+}
+}
+
+/*
+output:
+LetC
+*/
